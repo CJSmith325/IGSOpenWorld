9161117e08286941dceb08ee1beca0eb99fe02d7
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2023-01-12 13:17:07 -0500

    Enemy is walking

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float range; //range of motion
+    public Transform centrePoint; //the area the agent wants to move around in
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance <= agent.stoppingDistance) //done with set path
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point))
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //easier to view
+                agent.SetDestination(point);
+            }
+        }
+    }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+        result = Vector3.zero;
+        return false;
+    }
+}
