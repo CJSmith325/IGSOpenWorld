ccf2f9ac9b55f18c82122ee9f4504d01ecdb014d
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2023-01-12 16:44:25 -0500

    Made health bar

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject noteUI;
+    public GameObject hud;
+    public GameObject inv;
+
+    public GameObject pickUpText;
+    public AudioSource pickUpSound;
+    public bool inReach;
+
+    void Start()
+    {
+        noteUI.SetActive(false);
+        hud.SetActive(true);
+        inv.SetActive(true);
+        pickUpText.SetActive(false);
+        Time.timeScale = 1;
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && inReach)
+        {
+            noteUI.SetActive(true);
+            pickUpSound.Play();
+            hud.SetActive(false);
+            inv.SetActive(false);
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void ExitButton()
+    {
+        noteUI.SetActive(false);
+        hud.SetActive(true);
+        inv.SetActive(true);
+        Time.timeScale = 1;
+        Cursor.visible = false;
+    }
+}
