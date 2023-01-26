aaa397701a1bad0a8c9d778ae7e568898f8a77e2
jrpsus <jpaesano@ltu.edu>
2023-01-26 15:44:00 -0500

    enemyhealth

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int ehealth;
+    public int edamage;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
