4014046d862a16c4a211d97afc4b9f82d3f1925f
Legendz253 <ijarvis@ltu.edu>
2023-01-31 14:16:26 -0500

    Respawn Script Testing

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform Player;
+    public Transform SpawnPoint;
+    private Vector3 SpawnLocation;
+    public KeyCode respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+       SpawnLocation = SpawnPoint.transform.position; //Sets Player SpawnLocation to Spawnpoint Position.
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(respawn)) //Calls Respawn Function
+        {
+            PlayerRespawn();
+        }
+
+    }
+    //Respawn Function
+    void PlayerRespawn()
+    {
+
+        Debug.Log("SpawnPoint: " + SpawnPoint.transform.position);
+
+        Debug.Log("TP");
+        Player.transform.position = SpawnLocation; //Sets player Position to SpawnLocations saved position.
+        Debug.Log("SpawnLocation: " + SpawnLocation + "\tplayerLocation: " + Player.transform.position);
+
+    }
+}
