fd4ba3f851f4503f854f0803f33df2939bec89e6
jrpsus <jpaesano@ltu.edu>
2023-02-02 15:01:49 -0500

    axe recolors

---
+++ Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    public int baseDamage;
+    public int durability;
+    public float attackSpeed;
+    public float attackRange;
+}
