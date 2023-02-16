f42923f921e40b727d41d2037c53cb5a491869b0
Chief-Beef <wschweri1@ltu.edu>
2023-02-16 15:21:50 -0500

    Player Weapon Switching
    Minecraft 1.45.3 The Sex Update

--- Assets/Scripts/WeaponSwitch.cs
+++ Assets/Scripts/WeaponSwitch.cs
@@ -4,49 +4,120 @@ using UnityEngine;
 
 public class WeaponSwitch : MonoBehaviour
 {
-    public GameObject object01;
-    public GameObject object02;
-    public GameObject object03;
-    public GameObject object04;
+
+    public int totalWeapons;
+    public int activeWeapon;
+
+    public KeyCode switchWeapon;
+
+    public GameObject[] weapons;
+    public GameObject axe1, axe2, axe3, dblAxe1, dblAxe2, dblAxe3, sword1, sword2, sword3, bigSword1, bigSword2, bigSword3;
+
 
     void Start()
     {
-        object01.SetActive(false);
-        object02.SetActive(false);
-        object03.SetActive(false);
-        object04.SetActive(false);
+        totalWeapons = 12;
+        activeWeapon = 0;
+
+        weapons = new GameObject[totalWeapons];
+
+        weapons[0] = axe1;
+        weapons[1] = axe2;
+        weapons[2] = axe3;
+        weapons[3] = dblAxe1;
+        weapons[4] = dblAxe2;
+        weapons[5] = dblAxe3;
+        weapons[6] = sword1;
+        weapons[7] = sword2;
+        weapons[8] = sword3;
+        weapons[9] = bigSword1;
+        weapons[10] = bigSword2;
+        weapons[11] = bigSword3;
+
+
+        weapons[0].SetActive(true);
+        weapons[1].SetActive(false);
+        weapons[2].SetActive(false);
+        weapons[3].SetActive(false);
+        weapons[4].SetActive(false);
+        weapons[5].SetActive(false);
+        weapons[6].SetActive(false);
+        weapons[7].SetActive(false);
+        weapons[8].SetActive(false);
+        weapons[9].SetActive(false);
+        weapons[10].SetActive(false);
+        weapons[11].SetActive(false);
+
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("1"))
+        if(Input.GetKeyDown(switchWeapon))
         {
-            object01.SetActive(true);
-            object02.SetActive(false);
-            object03.SetActive(false);
-            object04.SetActive(false);
+            SwitchWeapons();
         }
-        if (Input.GetButtonDown("2"))
+        else if (Input.GetButtonDown("1"))
         {
-            object01.SetActive(false);
-            object02.SetActive(true);
-            object03.SetActive(false);
-            object04.SetActive(false);
+            SwitchWeapons(0);
         }
-        if (Input.GetButtonDown("3"))
+        else if (Input.GetButtonDown("2"))
         {
-            object01.SetActive(false);
-            object02.SetActive(false);
-            object03.SetActive(true);
-            object04.SetActive(false);
+            SwitchWeapons(3);
         }
-        if (Input.GetButtonDown("4"))
+        else if (Input.GetButtonDown("3"))
         {
-            object01.SetActive(false);
-            object02.SetActive(false);
-            object03.SetActive(false);
-            object04.SetActive(true);
+            SwitchWeapons(6);
+        }
+        else if (Input.GetButtonDown("4"))
+        {
+            SwitchWeapons(9);
         }
 
+    }
+
+    public void SwitchWeapons()
+    {
+        activeWeapon = (activeWeapon + 1) % totalWeapons;
+
+        for (int i = 0; i < totalWeapons; i++)
+        {
+            if (i == activeWeapon)
+            {
+                //Set the chosen Weapon to active
+                weapons[i].SetActive(true);
+                //crosshairs[i].enabled = true;
+                //Txt[i].enabled = true;
+            }
+            else
+            {
+                //set all other Weapons to inactive
+                weapons[i].SetActive(false);
+                //crosshairs[i].enabled = false;
+                //Txt[i].enabled = false;
+            }
+        }
+    }
+
+    public void SwitchWeapons(int num)
+    {
+        activeWeapon = num;
+
+        for (int i = 0; i < totalWeapons; i++)
+        {
+            if (i == activeWeapon)
+            {
+                //Set the chosen Weapon to active
+                weapons[i].SetActive(true);
+                //crosshairs[i].enabled = true;
+                //Txt[i].enabled = true;
+            }
+            else
+            {
+                //set all other Weapons to inactive
+                weapons[i].SetActive(false);
+                //crosshairs[i].enabled = false;
+                //Txt[i].enabled = false;
+            }
+        }
     }
 }
