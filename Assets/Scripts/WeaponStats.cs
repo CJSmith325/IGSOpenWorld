d87764d1f519bee3d82f4742fbe2af023d57001b
Chief-Beef <wschweri1@ltu.edu>
2023-02-16 16:10:19 -0500

    Updated WeaponStats Script
    will add function code later

--- Assets/Scripts/WeaponStats.cs
+++ Assets/Scripts/WeaponStats.cs
@@ -4,8 +4,71 @@ using UnityEngine;
 
 public class WeaponStats : MonoBehaviour
 {
-    public int baseDamage;
-    public int durability;
-    public float attackSpeed;
-    public float attackRange;
+
+    //basic attack vars
+    public float baseDamage, durability, attackSpeed, attackRange;
+
+    //damage mods
+    public float critMod, critChance, critDamage;
+
+    //attack timers
+    public float attackTime, attackTimer;
+
+    public bool isSword;
+
+    private void Start()
+    {
+ 
+        critDamage = baseDamage * critMod;
+        attackTime = 1f / attackSpeed;
+        attackTimer = attackTime;
+
+    }
+
+
+    private void Update()
+    {
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (isSword)
+                SwordSwing();
+            else
+                AxeSwing();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            if (isSword)
+                SwordChop();
+            else
+                AxeChop();
+        }
+
+
+    }
+
+    public void SwordSwing()
+    {
+        //sword swing code
+    }
+
+    public void SwordChop()
+    {
+        //sword chop code
+    }
+
+    public void AxeSwing()
+    {
+        //axe swing code
+    }
+
+    public void AxeChop()
+    {
+        //axe chop code
+    }
+
+
+
+
+
 }
