6354f35b3a800cc3775953d71a0d19b0629f8a4d
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2023-03-14 11:49:43 -0400

    Fixed door and elevator

---
+++ Assets/Scripts/ButtonActivate2.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivate2 : MonoBehaviour
+{
+    public GameObject text;
+
+    public bool powerIsOn;
+
+    private bool inReach;
+
+    public Animator ANI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach" && !powerIsOn)
+        {
+            inReach = true;
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            text.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && inReach)
+        {
+            powerIsOn = true;
+        }
+
+        if (powerIsOn)
+        {
+            ANI.SetBool("GlassDoorOpen2", true);
+            ANI.SetBool("idleGlassDoor2", true);
+
+            inReach = false;
+            text.SetActive(false);
+        }
+
+        if (!powerIsOn)
+        {
+            ANI.SetBool("idleGlassDoor2", false);
+            ANI.SetBool("GlassDoorOpen2", true);
+        }
+    }
+}
