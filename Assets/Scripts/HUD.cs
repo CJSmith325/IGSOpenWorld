ad0bf8452df7d76551b872ffe28d0b5d01c6fb5a
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2023-04-20 17:20:10 -0400

    Revert "UI Changes"
    This reverts commit a77b3699c155769a75c5a930da4d642f6547990f.

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -50,7 +50,6 @@ public class HUD : MonoBehaviour
 
         fill = (PlayerHealth * 100 / PlayerMaxHealth);
         HealthImage.fillAmount = fill / 100;
-        //HealthImage.fillAmount = Mathf.Lerp(fill/100, (PlayerHealth * 100 / PlayerMaxHealth), Time.deltaTime * .8f);
 
         healthPct = (float)PlayerHealth / (float)PlayerMaxHealth;
 
